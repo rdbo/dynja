3c410ec44dc43b1acebfef4d67cf4ec5a9ec684d
rdbo <rdbodev@gmail.com>
2024-01-11 05:45:06 -0300

    added way of using only minijinja

--- dynja/src/lib.rs
+++ dynja/src/lib.rs
@@ -1,27 +1,35 @@
-use minijinja::Environment;
-use std::sync::{Mutex, OnceLock};
-
-#[cfg(debug_assertions)]
-pub use dynja_derive::Template;
-
-#[cfg(not(debug_assertions))]
+// Re-export askama template, if askama_release is enabled and the build type is release
+#[cfg(all(feature = "askama_release", not(debug_assertions)))]
 pub use askama::Template;
 
-pub use minijinja;
+// Declare minijinja wrapper on debug, or if askama_release isn't enabled
+#[cfg(any(debug_assertions, not(feature = "askama_release")))]
+mod dynja_minijinja {
+    use minijinja::Environment;
+    use std::sync::{Mutex, OnceLock};
 
-pub trait TemplateFile {
-    const PATH: &'static str; // NOTE: this is not the actual path of the template.
-                              // It's just the name of the variable taken in the #[template] macro,
-                              // just like with 'askama'.
+    pub use dynja_derive::Template;
+
+    pub use minijinja;
+
+    pub trait TemplateFile {
+        const PATH: &'static str; // NOTE: this is not the actual path of the template.
+                                  // It's just the name of the variable taken in the #[template] macro,
+                                  // just like with 'askama'.
+    }
+
+    pub fn environment() -> &'static Mutex<Environment<'static>> {
+        static ENV: OnceLock<Mutex<Environment>> = OnceLock::new();
+        ENV.get_or_init(|| {
+            let mut env = Environment::new();
+            env.set_loader(minijinja::path_loader("templates"));
+
+            let mutex = Mutex::new(env);
+            mutex
+        })
+    }
 }
 
-pub fn environment() -> &'static Mutex<Environment<'static>> {
-    static ENV: OnceLock<Mutex<Environment>> = OnceLock::new();
-    ENV.get_or_init(|| {
-        let mut env = Environment::new();
-        env.set_loader(minijinja::path_loader("templates"));
-
-        let mutex = Mutex::new(env);
-        mutex
-    })
-}
+// Re-export minijinja wrapper
+#[cfg(any(debug_assertions, not(feature = "askama_release")))]
+pub use dynja_minijinja::*;
--- dynja_tests/src/main.rs
+++ dynja_tests/src/main.rs
@@ -6,8 +6,29 @@ struct MyTemplate<'a> {
     name: &'a str,
 }
 
+// Current way of setting filters (not seamless unfortunately)
+#[cfg(not(debug_assertions))]
+pub mod filters {
+    pub fn myfilter(_input: &str) -> Result<String, askama::Error> {
+        Ok("Filter Applied (askama)".into())
+    }
+}
+
+#[cfg(debug_assertions)]
+fn setup_filters() {
+    let env = dynja::environment();
+    env.lock()
+        .unwrap()
+        .add_filter("myfilter", |_input: String| "Filter Applied (minijinja)");
+}
+
+#[cfg(not(debug_assertions))]
+fn setup_filters() {}
+
 fn main() {
     println!("dynja tests");
+
+    setup_filters();
 
     let template = MyTemplate { name: "Test" };
     println!("Template: {:?}", template);
